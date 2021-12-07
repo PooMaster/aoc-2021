9efe9051d3841d0663353302c4d55128f0c2b308
PooMaster <peter.d.curtis@gmail.com>
2021-12-07 01:20:20 -0500

    Day 7 part 2

--- day7/main.py
+++ day7/main.py
@@ -2,9 +2,22 @@ from statistics import median
 
 if __name__ == '__main__':
     content: str = open('input.txt').read()
-    values: list[int] = [int(v) for v in content.strip().split(',')]
+    crab_locations: list[int] = [int(v) for v in content.strip().split(',')]
 
-    median_value: int = int(median(values))
-    fuel_required: int = sum(abs(v - median_value) for v in values)
+    # Part 1
+    # Does this always work??
+    median_location: int = int(median(crab_locations))
+    fuel_required: int = sum(abs(l - median_location) for l in crab_locations)
     print('Part 1:')
     print(fuel_required)
+
+    # Part 2
+    # Just go for brute force i guess
+    def summation(n: int) -> int:
+        "Sum of integers from 1 to n"
+        return n * (n + 1) // 2
+    fuel_required = [
+        sum(summation(abs(crab - pos)) for crab in crab_locations)
+        for pos in range(len(crab_locations))
+    ]
+    print(min(fuel_required))
